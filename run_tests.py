5f76c30ec411df571e994964b979738821b5b23c
Sean Nieuwoudt <sean@apollo.black>
2025-04-29 15:14:06 +0200

    Moving the tests to the top level directory

--- run_tests.py
+++ run_tests.py
@@ -2,6 +2,6 @@ import unittest
 
 if __name__ == '__main__':
     test_loader = unittest.TestLoader()
-    test_suite = test_loader.discover('src/ydnatl/tests')
+    test_suite = test_loader.discover('tests')
     test_runner = unittest.TextTestRunner()
     test_runner.run(test_suite) 
